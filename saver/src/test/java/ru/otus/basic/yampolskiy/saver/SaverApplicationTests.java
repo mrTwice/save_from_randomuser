e927f9fd943bb6e6534ba58ecceea0ff694713bd
mrTwice <yampolskiy86@gmail.com>
2024-08-24 15:40:27 +0300

    add spring boot

---
+++ saver/src/test/java/ru/otus/basic/yampolskiy/saver/SaverApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.basic.yampolskiy.saver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
